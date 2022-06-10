b00986903b5c6153be60e29ebbf8eaa740197dae
nurceylan <nurceylan.karasu@trendyol.com>
2022-06-10 09:50:26 +0300

    initial commit

---
+++ src/main/java/com/example/katastdd2/KatasTdd2Application.java
@@ -0,0 +1,13 @@
+package com.example.katastdd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatasTdd2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatasTdd2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/katastdd2/KatasTdd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.katastdd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatasTdd2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
